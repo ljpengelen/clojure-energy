d5bb0d47330c52eec87b5fd5072a8f44cfb8c154
Luc Engelen <luc@kabisa.nl>
2019-12-24 21:15:31 +0100

    Update project URL

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-energy "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/ljpengelen/clojure-energy"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
